93f5145ca8f91e561c5260217109e5b872fd4a6b
ivanna-balanyuk <ivannabalanyuk@yandex.ru>
2024-06-02 22:27:55 +0300

    fix: внести правки для деплоя без докера

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,10 +10,10 @@ module.exports = {
   deploy: {
     production: {
       user: DEPLOY_USER,
-      host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
+      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm ci && npm run build',
     },
   },
